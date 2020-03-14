c1b4364f44510c7ce5f5a7ace5ce11e32448e6c2
Ichrak92 <56369109+Ichrak92@users.noreply.github.com>
2020-03-14 15:36:17 +0100

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Todolist from './Todolist';
+export default class App extends React.Component{
+  render(){
+    return(
+      <div>
+      <Todolist/>
+      </div>
+    );
+    
+  }
+}
+
+
